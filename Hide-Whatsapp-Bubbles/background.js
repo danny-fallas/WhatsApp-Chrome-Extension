a820cee3b95306b12186112f381e54bcc73619b0
Danny Fallas <dann_1990@hotmail.com>
2017-05-04 13:09:56 -0600

    Fixed toogle functionallity

---
+++ Hide-Whatsapp-Bubbles/background.js
@@ -0,0 +1,27 @@
+var toggle = false;
+
+function startService(tab) {
+    chrome.browserAction.setIcon({ path: "128.png" });
+    chrome.browserAction.setTitle({ title: "Turn OFF" });
+    chrome.tabs.executeScript(tab.id, { code: "start()" }, callback);
+}
+
+function stopService(tab) {
+    chrome.browserAction.setIcon({ path: "32.png" });
+    chrome.browserAction.setTitle({ title: "Turn ON" });
+    chrome.tabs.executeScript(tab.id, { code: "stop()" }, callback);
+}
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+    toggle = !toggle;
+    if (toggle) {
+        startService(tab);
+    }
+    else {
+        stopService(tab);
+    }
+});
+
+function callback(results) {
+    console.log(results);
+}
