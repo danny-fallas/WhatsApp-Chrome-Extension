ae4ad0a09a30493d9ccf82c03dae174c4ea5a527
Danny Fallas <dann_1990@hotmail.com>
2017-05-13 17:08:00 -0600

    Better process management in background.js, added return object to content script

--- Hide-Whatsapp-Bubbles/background.js
+++ Hide-Whatsapp-Bubbles/background.js
@@ -1,16 +1,18 @@
 var isActive = false;
-var _tabId = '';
+var activeOnce = false;
+var _tabId = null;
 
 function startService() {
     chrome.browserAction.setIcon({ path: '128.png' });
     chrome.browserAction.setTitle({ title: 'Turn OFF' });
-    chrome.tabs.executeScript(_tabId, { code: 'start()' }, executeScriptCallback);
+    runScript({ code: 'start()' });
+    activeOnce = true;
 }
 
 function stopService() {
     chrome.browserAction.setIcon({ path: '32.png' });
     chrome.browserAction.setTitle({ title: 'Turn ON' });
-    chrome.tabs.executeScript(_tabId, { code: 'stop()' }, executeScriptCallback);
+    runScript({ code: 'stop()' });
 }
 
 function changeStatus(tabId) {
@@ -31,13 +33,27 @@ chrome.browserAction.onClicked.addListener(function (tab) {
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     //TO DO: fix this functionality, the onUpdate method executes everytime any tab updates
-    if ((changeInfo.title && changeInfo.title.toLowerCase().includes('whatsapp'))
+    //If the service was used once it will keep it on
+    if (activeOnce && (changeInfo.title && changeInfo.title.toLowerCase().includes('whatsapp'))
         || (changeInfo.url && changeInfo.url.toLowerCase().includes('whatsapp'))) {
-        isActive = false;
-        stopService();
+        isActive = true;
+        startService();
     }
 });
 
-function executeScriptCallback(response) {
-    console.debug(response);
+function runScript(code) {
+    function callback(response) {
+        console.debug(response);
+    }
+
+    try {
+        if (code.code)
+            chrome.tabs.executeScript(_tabId, code, callback);
+        else if (code.file)
+            chrome.tabs.executeScript(_tabId, code);
+    } catch (e) {
+        console.error('ERROR: {chrome.tabs.executeScript}' + e);
+    }
 }
+
+runScript({ file: 'contentscript.js' });
--- Hide-Whatsapp-Bubbles/contentscript.js
+++ Hide-Whatsapp-Bubbles/contentscript.js
@@ -1,12 +1,14 @@
 var run;
 function start() {
     run = setInterval(hide_bubbles, 100);
-    console.log("...Started");
+    console.log('...Started');
+    return { service: 'Running' };
 }
 
 function stop() {
     clearInterval(run);
-    console.log("...Stopped");
+    console.log('...Stopped');
+    return { service: 'Stopped' };
 }
 
 function hide_bubbles() {
@@ -23,16 +25,16 @@ function hide_bubbles() {
 
         var imgElement = element.getElementsByTagName('IMG')[0]; //First image in the bubble
 
-        if (imgElement &&  imgElement.tagName && imgElement.tagName === 'IMG')
+        if (imgElement && imgElement.tagName && imgElement.tagName === 'IMG')
             imgElement.src = url;
         else
             element.innerHTML = innerImg;
 
-        /*element.addEventListener("mouseover", function () {
+        /*element.addEventListener('mouseover', function () {
             document.getElementsByClassName('pane-list-user')[0].getElementsByTagName('img')[0].src = original[i];
         }, false);
  
-        element.addEventListener("mouseout", function () {
+        element.addEventListener('mouseout', function () {
             document.getElementsByClassName('pane-list-user')[0].getElementsByTagName('img')[0].src = url;
         }, false);*/
         //elements[i].style.display = 'none';
